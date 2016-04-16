a1c61471a84ad42f829424314088004dcce45217
Hares Faiez <faiez.hrs@gmail.com>
2016-04-16 15:20:29 +0100

    Add "missed concert updates" stub implementation

--- src/functionaltest/groovy/stories/event/Updates.groovy
+++ src/functionaltest/groovy/stories/event/Updates.groovy
@@ -1,2 +1,27 @@
 package stories.event
 
+import cucumber.api.PendingException
+
+this.metaClass.mixin(cucumber.api.groovy.Hooks)
+this.metaClass.mixin(cucumber.api.groovy.EN)
+
+Given(~/^there is a concert tonight$/) { ->
+    throw new PendingException()
+}
+
+Given(~/^Emma wants to attend it$/) { ->
+    throw new PendingException()
+}
+
+Given(~/^she is unable to do that$/) { ->
+    throw new PendingException()
+}
+
+When(~/^the concert begins$/) { ->
+    throw new PendingException()
+}
+
+Then(~/^she should receive all the updates of attendees about it$/) { ->
+    throw new PendingException()
+}
+
