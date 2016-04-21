5cca69456ea15800f47e0c9f51a7a961530680bf
Hares Faiez <faiez.hrs@gmail.com>
2016-04-21 22:40:44 +0100

    Extract empty collection creation in attendees test

--- src/test/java/stories/event/AttendeesTest.java
+++ src/test/java/stories/event/AttendeesTest.java
@@ -2,6 +2,8 @@ package stories.event;
 
 import org.junit.Test;
 
+import java.util.Collection;
+
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -10,13 +12,17 @@ public class AttendeesTest {
 
     @Test
     public void noOneAttendeeFactory() {
-        assertEquals(new Attendees(asList()), Attendees.noOne());
+        assertEquals(new Attendees(emptyCollection()), Attendees.noOne());
     }
 
     @Test
     public void equality() {
-        assertEquals(new Attendees(asList()), new Attendees(asList()));
-        assertNotEquals(new Attendees(asList()), null);
-        assertNotEquals(new Attendees(asList()), new Object());
+        assertEquals(new Attendees(emptyCollection()), new Attendees(emptyCollection()));
+        assertNotEquals(new Attendees(emptyCollection()), null);
+        assertNotEquals(new Attendees(emptyCollection()), new Object());
+    }
+
+    private Collection emptyCollection() {
+        return asList();
     }
 }
