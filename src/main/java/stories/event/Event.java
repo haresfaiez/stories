c2ed9d634447f7f7a8c43a6d1effcc939bcd57e7
Hares Faiez <faiez.hrs@gmail.com>
2016-04-21 21:11:30 +0100

    Add Event equality

--- src/main/java/stories/event/Event.java
+++ src/main/java/stories/event/Event.java
@@ -13,15 +13,33 @@ public class Event {
         this.time = time;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (null == o)             return Boolean.FALSE;
+        if (!(o instanceof Event)) return Boolean.FALSE;
+        Event other = (Event) o;
+        return other.hasId(id);
+    }
+
+    private Boolean hasId(Long otherId) {
+        return id.equals(otherId);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        throw new RuntimeException("toString not implemented on Event");
+    }
+
     public LocalDateTime time() {
         return time;
     }
 
     public String title() {
         return title;
-    }
-
-    public long id() {
-        return id;
     }
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -5,12 +5,25 @@ import java.time.LocalDateTime;
 import java.time.Month;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class EventTest {
+
     @Test
-    public void construction() {
+    public void eventEquality() {
+        Event concert      = new Event(1L, "Concert title", someTime());
+        Event sameConcert  = new Event(1L, "Concert title", someTime());
+        Event otherConcert = new Event(2L, "Concert title", someTime());
+
+        assertEquals   (sameConcert,  concert);
+        assertNotEquals(otherConcert, concert);
+        assertNotEquals(concert,      null);
+        assertNotEquals(concert,      new Object());
+    }
+
+    @Test
+    public void eventShouldHaveATitleAndATime() {
         Event concert = new Event(1L, "Concert title", someTime());
-        assertEquals(1L,              concert.id());
         assertEquals("Concert title", concert.title());
         assertEquals(someTime(),      concert.time());
     }
