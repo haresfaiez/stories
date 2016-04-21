be6037f700a45976f2b5e26c2891ea2c5e97f92f
Hares Faiez <faiez.hrs@gmail.com>
2016-04-21 22:39:41 +0100

    Test Attendees with no one attendee factory

--- src/test/java/stories/event/AttendeesTest.java
+++ src/test/java/stories/event/AttendeesTest.java
@@ -7,9 +7,15 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 public class AttendeesTest {
+
+    @Test
+    public void noOneAttendeeFactory() {
+        assertEquals(new Attendees(asList()), Attendees.noOne());
+    }
+
     @Test
     public void equality() {
-        assertEquals   (new Attendees(asList()), new Attendees(asList()));
+        assertEquals(new Attendees(asList()), new Attendees(asList()));
         assertNotEquals(new Attendees(asList()), null);
         assertNotEquals(new Attendees(asList()), new Object());
     }
