3a39394792b779b0b4854d62a33ae51bacb205fe
Hares Faiez <faiez.hrs@gmail.com>
2016-04-22 09:38:10 +0100

    Add PersonUpdate

---
+++ src/main/java/stories/event/PersonUpdate.java
@@ -0,0 +1,13 @@
+package stories.event;
+
+import java.time.LocalDateTime;
+
+public class PersonUpdate {
+    protected LocalDateTime time;
+    protected String        message;
+
+    public PersonUpdate(LocalDateTime time, String message) {
+        this.time    = time;
+        this.message = message;
+    }
+}
---
+++ src/test/java/stories/event/PersonUpdateTest.java
@@ -0,0 +1,22 @@
+package stories.event;
+
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+
+import static org.junit.Assert.assertEquals;
+
+public class PersonUpdateTest {
+
+    @Test
+    public void hasATimeAndAMessage() {
+        PersonUpdate update =  new PersonUpdate(someTime(), "Some message");
+        assertEquals(someTime(),     update.time);
+        assertEquals("Some message", update.message);
+    }
+
+    private LocalDateTime someTime() {
+        return LocalDateTime.of(2015, Month.APRIL, 19, 20, 30);
+    }
+}
