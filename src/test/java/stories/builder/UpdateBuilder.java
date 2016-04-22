3f7f1bfcd319c6f89e2e73e45a57d6c45577fae8
Hares Faiez <faiez.hrs@gmail.com>
2016-04-22 10:29:19 +0100

    Remove dependency to current time

--- src/test/java/stories/builder/UpdateBuilder.java
+++ src/test/java/stories/builder/UpdateBuilder.java
@@ -3,15 +3,21 @@ package stories.builder;
 import stories.event.EventUpdate;
 import stories.event.PersonUpdate;
 
-import static java.time.LocalDateTime.now;
+import java.time.LocalDateTime;
+import java.time.Month;
+
 import static stories.builder.PersonBuilder.bill;
 
 public class UpdateBuilder {
     public static PersonUpdate somePersonUpdate() {
-       return PersonUpdate.at(now(), "Some message");
+       return PersonUpdate.at(someTime(), "Some message");
     }
 
     public static EventUpdate someEventUpdate() {
         return new EventUpdate(bill(1L), somePersonUpdate());
+    }
+
+    public static LocalDateTime someTime() {
+        return LocalDateTime.of(2015, Month.APRIL, 19, 20, 30);
     }
 }
