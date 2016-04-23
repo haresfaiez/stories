6f9a1df5adb6177cc25a5b49f5c7fe915d40cb1e
Hares Faiez <faiez.hrs@gmail.com>
2016-04-23 21:21:01 +0100

    Make EventStatement constructor protected
    The constructor is used only for tests

--- src/main/java/stories/event/EventStatement.java
+++ src/main/java/stories/event/EventStatement.java
@@ -6,19 +6,19 @@ class EventStatement {
     protected String        title;
     protected LocalDateTime time;
 
-    public EventStatement(LocalDateTime time,
+    protected EventStatement(LocalDateTime time,
                           String title) {
         this.time  = time;
         this.title = title;
     }
 
-    public static EventStatement at(LocalDateTime time,
-                                    String title) {
-        return new EventStatement(time, title);
-    }
-
     @Override
     public String toString() {
         return String.format("%s, %s", title, time);
+    }
+
+    public static EventStatement at(LocalDateTime time,
+                                    String title) {
+        return new EventStatement(time, title);
     }
 }
