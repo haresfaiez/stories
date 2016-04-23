71f64dbf9f9d7a23d0148dd42046da36606c35d0
Hares Faiez <faiez.hrs@gmail.com>
2016-04-23 21:46:54 +0100

    Extract AttendeeUpdate heterogeneous test cases

--- src/test/java/stories/event/AttendeeUpdateTest.java
+++ src/test/java/stories/event/AttendeeUpdateTest.java
@@ -8,9 +8,19 @@ import static org.junit.Assert.assertNotEquals;
 public class AttendeeUpdateTest {
 
     @Test
+    public void equalityWithSameMessage() {
+        assertEquals(AttendeeUpdate.from("Some message"),
+                     AttendeeUpdate.from("Some message"));
+    }
+
+    @Test
+    public void equalityWithDifferentMessage() {
+        assertNotEquals(AttendeeUpdate.from("Some message"),
+                        AttendeeUpdate.from("An other message"));
+    }
+
+    @Test
     public void equality() {
-        assertEquals   (AttendeeUpdate.from("Some message"),
-                        AttendeeUpdate.from("Some message"));
         assertNotEquals(AttendeeUpdate.from("Some message"), null);
         assertNotEquals(AttendeeUpdate.from("Some message"), new Object());
     }
