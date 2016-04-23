710c287bd64faea67bd6b0f42f60cdac0bc5f087
Hares Faiez <faiez.hrs@gmail.com>
2016-04-23 22:03:31 +0100

    Extract heterogeneous test cases in Attendee test

--- src/test/java/stories/person/AttendeeTest.java
+++ src/test/java/stories/person/AttendeeTest.java
@@ -8,14 +8,30 @@ import static org.junit.Assert.assertNotEquals;
 public class AttendeeTest {
 
     @Test
-    public void equality() {
-        Attendee bill        = new Attendee(new Person(1L, "Bill"));
-        Attendee sameBill    = new Attendee(new Person(1L, "Bill"));
-        Attendee anotherBill = new Attendee(new Person(82L, "Bill"));
+    public void equalityWithDifferentPerson() {
+        Attendee anAttendee         = Attendee.from(personWithId(1L));
+        Attendee aDifferentAttendee = Attendee.from(personWithId(892L));
 
-        assertEquals   (sameBill,    bill);
-        assertNotEquals(anotherBill, bill);
-        assertNotEquals(bill,        null);
-        assertNotEquals(bill,        new Object());
+        assertNotEquals(anAttendee, aDifferentAttendee);
+    }
+
+    @Test
+    public void equalityWithSamePerson() {
+        Attendee anAttendee      = Attendee.from(personWithId(1L));
+        Attendee theSameAttendee = Attendee.from(personWithId(1L));
+
+        assertEquals(anAttendee, theSameAttendee);
+    }
+
+    @Test
+    public void equalityTowardNullAndObject() {
+        Attendee anAttendee = Attendee.from(personWithId(1L));
+
+        assertNotEquals(anAttendee, null);
+        assertNotEquals(anAttendee, new Object());
+    }
+
+    private Person personWithId(Long id) {
+        return new Person(id, "A person of that person");
     }
 }
