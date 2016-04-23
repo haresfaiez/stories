671aadc0f13344121da743875f0913b0dd4439ba
Hares Faiez <faiez.hrs@gmail.com>
2016-04-23 21:42:56 +0100

    Extract Attendees test cases

--- src/test/java/stories/event/AttendeesTest.java
+++ src/test/java/stories/event/AttendeesTest.java
@@ -1,9 +1,12 @@
 package stories.event;
 
 import org.junit.Test;
+import stories.person.Attendee;
+import stories.person.Person;
 
-import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -11,18 +14,38 @@ import static org.junit.Assert.assertNotEquals;
 public class AttendeesTest {
 
     @Test
-    public void noOneAttendeeFactory() {
-        assertEquals(new Attendees(empty()), Attendees.noOne());
+    public void noOneAttendeeEqualsEmptyCollection() {
+        assertEquals(new Attendees(anEmptySet()), Attendees.noOne());
     }
 
     @Test
-    public void equality() {
-        assertEquals   (new Attendees(empty()), new Attendees(empty()));
-        assertNotEquals(new Attendees(empty()), null);
-        assertNotEquals(new Attendees(empty()), new Object());
+    public void equalityWithEqualCollections() {
+        assertEquals(new Attendees(anEmptySet()), new Attendees(anEmptySet()));
     }
 
-    private Collection empty() {
-        return Collections.EMPTY_LIST;
+    @Test
+    public void equalityWithDifferentCollections() {
+        assertNotEquals(new Attendees(anEmptySet()),
+                        new Attendees(notAnEmptySet()));
+    }
+
+    @Test
+    public void equalityTowardNullAndObject() {
+        assertNotEquals(new Attendees(anEmptySet()), null);
+        assertNotEquals(new Attendees(anEmptySet()), new Object());
+    }
+
+    private Set notAnEmptySet() {
+        Set result = new HashSet();
+        result.add(Attendee.from(arbitraryPerson()));
+        return result;
+    }
+
+    private Person arbitraryPerson() {
+        return new Person(1L, "Some name");
+    }
+
+    private Set anEmptySet() {
+        return Collections.emptySet();
     }
 }
