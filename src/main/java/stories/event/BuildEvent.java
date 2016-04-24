54fe83ed46f5fd542c9211310cba43649fbe6dd3
Hares Faiez <faiez.hrs@gmail.com>
2016-04-24 16:34:01 +0100

    Make building methods visible outside their package

--- src/main/java/stories/event/BuildEvent.java
+++ src/main/java/stories/event/BuildEvent.java
@@ -2,7 +2,7 @@ package stories.event;
 
 import java.time.LocalDateTime;
 
-class BuildEvent {
+public class BuildEvent {
     private Event product;
 
     private BuildEvent(Long identity) {
@@ -11,31 +11,31 @@ class BuildEvent {
                             Updates.none());
     }
 
-    protected BuildEvent entitled(String title) {
+    public BuildEvent entitled(String title) {
         product.specification = product.specification.withTitle(title);
         return this;
     }
 
-    protected BuildEvent at(LocalDateTime newTime) {
+    public BuildEvent at(LocalDateTime newTime) {
         product.specification = product.specification.withTime(newTime);
         return this;
     }
 
-    protected BuildEvent withAttendees(Attendees attendees) {
+    public BuildEvent withAttendees(Attendees attendees) {
         product.specification = product.specification.withAttendees(attendees);
         return this;
     }
 
-    protected BuildEvent withUpdates(Updates updates) {
+    public BuildEvent withUpdates(Updates updates) {
         product.updates = updates;
         return this;
     }
 
-    protected Event product() {
+    public Event product() {
         return product;
     }
 
-    protected static BuildEvent identifiedBy(Long identity) {
+    public static BuildEvent identifiedBy(Long identity) {
         return new BuildEvent(identity);
     }
 }
