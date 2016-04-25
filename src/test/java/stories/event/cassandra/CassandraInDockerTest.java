8bb01e2741e266d674d5f08c4652dcfbb0a56d05
Hares Faiez <faiez.hrs@gmail.com>
2016-04-25 08:55:30 +0100

    Test Spark result to JavaRDD mapping

--- src/test/java/stories/event/cassandra/CassandraInDockerTest.java
+++ src/test/java/stories/event/cassandra/CassandraInDockerTest.java
@@ -4,6 +4,9 @@ import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.datastax.spark.connector.cql.CassandraConnector;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.junit.Test;
 
 import java.util.UUID;
@@ -11,11 +14,28 @@ import java.util.UUID;
 import static org.junit.Assert.assertEquals;
 
 public class CassandraInDockerTest {
+    String cassandraHost = "172.17.0.1";
+    private String sparkMaster = "local";
 
     @Test
+    public void convertToJavaRDD() {
+        SparkConf conf = new SparkConf();
+        conf.setAppName("Java API demo");
+        conf.setMaster(sparkMaster);
+        conf.set("spark.cassandra.connection.host", cassandraHost);
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        CassandraConnector connector = CassandraConnector.apply(sc.getConf());
+        Session session = connector.openSession();
+        ResultSet result = session.execute("SELECT * FROM Stories.Event");
+        Row first = result.iterator().next();
+        assertEquals(first.getUUID("id"), UUID.fromString("32b0a8e0-0a3d-11e6-8cf0-2d237e461979"));
+        sc.stop();
+    }
+
+    @Test
+
     public void findEventById() {
-        String containerIp = "172.17.0.1";
-        Cluster cluster = Cluster.builder().addContactPoint(containerIp).build();
+        Cluster cluster = Cluster.builder().addContactPoint(cassandraHost).build();
         Session session = cluster.connect("Stories");
         ResultSet result = session.execute("SELECT * FROM Event");
         Row first = result.iterator().next();
