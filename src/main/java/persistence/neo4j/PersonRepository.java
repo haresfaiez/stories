46aed05990bcb9667c8ea3b91ae434e555b61de6
Hares Faiez <faiez.hrs@gmail.com>
2016-05-11 17:20:08 +0100

    Use helpers in Person repository

--- src/main/java/persistence/neo4j/PersonRepository.java
+++ src/main/java/persistence/neo4j/PersonRepository.java
@@ -7,9 +7,9 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import stories.person.Person;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PersonRepository {
     private Neo4jContext authentication;
@@ -18,53 +18,51 @@ public class PersonRepository {
         this.authentication = authentication;
     }
 
-    public static PersonRepository using(Neo4jContext authentication) {
-        return new PersonRepository(authentication);
+    public Person personWithId(UUID target) {
+        return Person.from(personsFrom(responseTo(retrievePersonQuery(target))).get(0));
     }
 
-    public Person personWithId(UUID target) {
-        String query = retrievePersonQuery(target);
-        return Person.from(dataFrom(responseTo(query), 0));
+    public Set<Person> personsFollowedBy(Person origin) {
+        String response = responseTo(followedByQuery(origin.id));
+        return personsFrom(response).stream()
+                                    .map(Person::from)
+                                    .collect(Collectors.toSet());
+    }
+
+    private String responseTo(String query) {
+        return Http.post(authentication.requestURI(), query)
+                   .header("Accept", "application/json; charset=UTF-8")
+                   .header("Content-Type", "application/json")
+                   .header("Authorization", authentication.authorization())
+                   .text();
     }
 
     private String retrievePersonQuery(UUID target) {
-        String queryTemplate = "MATCH (target: Person {id: \\\"%s\\\"}) RETURN target";
-        String query = String.format(queryTemplate, target);
-        return String.format("{\"query\" : \"%s\", \"params\" : { }}", query);
+        String template = "MATCH (target: Person {id: \\\"%s\\\"}) RETURN target";
+        return queryFrom(String.format(template, target));
     }
 
-    public JSONObject dataFrom(String response, int index) {
+    private String followedByQuery(UUID origin) {
+        String template = "MATCH (:Person {id: \\\"%s\\\"}) -[:FOLLOWS]->(followed) RETURN followed";
+        return queryFrom(String.format(template, origin));
+    }
+
+    private String queryFrom(String template) {
+        return String.format("{\"query\" : \"%s\", \"params\" : { }}", template);
+    }
+
+    public List<JSONObject> personsFrom(String response) {
         try {
             JSONParser parser = new JSONParser();
             JSONArray responseData = (JSONArray) ((JSONObject) parser.parse(response)).get("data");
-            JSONObject result = (JSONObject) ((JSONArray) responseData.get(index)).get(0);
-            return (JSONObject) result.get("data");
+            Stream result = responseData.stream().map(each -> ((JSONObject) ((JSONArray) each).get(0)).get("data"));
+            return (List<JSONObject>) result.collect(Collectors.toList());
         } catch (ParseException e) {
             throw new RuntimeException("Response not formatted as expected");
         }
     }
 
-    public Set<Person> personsFollowedBy(Person origin) {
-        String query = followedByQuery(origin.id);
-        String responseRaw = responseTo(query);
-        HashSet<Person> result = new HashSet<>();
-        result.add(Person.from(dataFrom(responseRaw, 0)));
-        result.add(Person.from(dataFrom(responseRaw, 1)));
-        return result;
-    }
-
-    private String responseTo(String query) {
-        return Http.post(authentication.requestURI(), query)
-                .header("Accept", "application/json; charset=UTF-8")
-                .header("Content-Type", "application/json")
-                .header("Authorization", authentication.authorization())
-                .text();
-    }
-
-    private String followedByQuery(UUID origin) {
-        String queryTemplate
-                = "MATCH (:Person {id: \\\"%s\\\"}) -[:FOLLOWS]->(followed) RETURN followed";
-        String query = String.format(queryTemplate, origin);
-        return String.format("{\"query\" : \"%s\", \"params\" : { }}", query);
+    public static PersonRepository using(Neo4jContext authentication) {
+        return new PersonRepository(authentication);
     }
 }
